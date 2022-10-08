629c52100606de9c2b1e3b71ee3833adf5aed244
undertaker28 <pavel.starovoytovvv@gmail.com>
2022-10-09 02:01:42 +0300

    Initial Commit

---
+++ MangekyouSharinganAnimation/MangekyouSharinganAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangekyouSharinganAnimationApp.swift
+//  MangekyouSharinganAnimation
+//
+//  Created by Pavel on 9.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MangekyouSharinganAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
